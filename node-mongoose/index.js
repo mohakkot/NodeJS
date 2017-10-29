3e59e5ec614e9cd259ed49ef56d9dd146446a525
Mohak Kothari <mohak_kothari@hotmail.com>
2017-10-28 21:00:26 -0700

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -10,18 +10,30 @@ const connect = mongoose.connect(url, {
 
 connect.then((db) => {
     console.log('Connected correctly to server');
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    });
-
-    newDish.save()
+    })
     .then((dish)=> {
         console.log(dish);
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set : { description: 'Updated Test'}
+        },{
+            new: true
+        })
+        .exec()
     })
-    .then((dishes)=> {
-        console.log(dishes);
+    .then((dish)=> {
+        console.log(dish);
+        dish.comments.push({
+            rating: 1,
+            comment: "I'm getting sick feeling!",
+            author: 'Me'
+        });
+        return dish.save();
+    })
+    .then((dish)=>{
+        console.log(dish)
         return db.collection('dishes').drop();
     })
     .then(()=>{
